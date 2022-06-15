220ab3499ffd4dfeedc9a18196c8bd5263400e35
sdmnny <sdmnny@gmail.com>
2022-06-14 20:38:23 -0400

    connected database

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+// Dependencies
 const express = require('express');
 const mysql = require('mysql12');
-const cTable = require("console.table");
+const cTable = require('console.table');
 
+// PORT
 const PORT = process.env.PORT || 3001;
 const app = express(); 
 
 
-// middleware
+// MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Connect To Database 
+const db = mysql.createConnection(
+    {
+      host: "localhost",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
